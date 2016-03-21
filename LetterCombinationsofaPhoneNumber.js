bfc3e9871a9bed0bdd8e27161d9049f079bafe52
wangdading <wangdading@xindong.com>
2016-03-21 19:33:33 +0800

    更改实现方法

--- LetterCombinationsofaPhoneNumber.js
+++ LetterCombinationsofaPhoneNumber.js
@@ -1,52 +1,37 @@
+/*
+思路：先选出字符串第一个字符对应的数组
+将剩下字符的数组一一和第一个字符对应的数组
+进行遍历生成字符串，保存到一个专门用来替代
+的数组，遍历完一次，生成的结果替代掉之前的
+第一个字符对应的数组，接着和下一个字符对应
+的数组进行遍历，一直重复到字符完
+*/
 /**
  * @param {string} digits
  * @return {string[]}
  */
-var getDataLength = function(index) {
-	if (index == -1) {
-		return -1;
-	}
-	else if (index == 0) {
-		return 1;
-	}
-	else if (index == 6 || index == 8) {
-		return 4;
-	}
-	else {
-		return 3;
-	}
-};
-
 var letterCombinations = function(digits) {
-  	var _Data = [
-  		['*'],
-  		['a','b','c'],
-  		['d','e','f'],
-  		['g','h','i'],
-  		['j','k','l'],
-  		['m','n','o'],
-  		['p','q','r','s'],
-  		['t','u','v'],
-  		['w','x','y','z']
-  	];
-  	var result = new Array();
-  	if (digits == null || digits == "" || digits.length == 0) {
-  		return result;
-  	}
-  	var first_value = 0,
-  		second_value = 0;
-  	if (digits.length == 1)
-  		first_value = parseInt(digits.charAt(0));
-  	else if (digits.length > 1) {
-  		first_value = parseInt(digits.charAt(0));
-  		second_value = parseInt(digits.charAt(1));
-  	}
-  	for (var i = 0; i < _Data[getDataLength(first_value)].length; i++) {
-  		var substring = _Data[i];
-  		for (var j = 0; j < _Data[getDataLength(second_value)].length; j++) {
-  			substring += _Data[j];
-  		}
-  		result.push(substring);
-  	}
-  	return result;
+  	var _Data = {
+  		"1":['*'],
+  		"2":['a','b','c'],
+  		"3":['d','e','f'],
+  		"4":['g','h','i'],
+  		"5":['j','k','l'],
+  		"6"['m','n','o'],
+  		"7":['p','q','r','s'],
+  		"8":['t','u','v'],
+  		"9":['w','x','y','z']
+  	};
+  	var First_array = map[digits[0]];
+    digits = digits.substr(1);
+    digits.splict('').foreach(function(digit){
+        var Tidai_array = [];
+        _Data[digit].foreach(function(letter){
+            Tidai_array.concat(First_array.map(function(item){
+                return item + letter;
+            }));
+        });
+        First_array = Tidai_array;
+    });
+    return First_array === undefined ? [] : First_array;
 };
