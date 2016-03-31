8a99845018a384de0b86ebd1f35c0d4336177136
George1994 <1414648814@users.noreply.github.com>
2016-03-31 10:44:00 +0800

    位运算计算除法：思路

---
+++ Divide Two Integers.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} dividend
+ * @param {number} divisor
+ * @return {number}
+ */
+ /*
+	思路：利用位计算实现
+	除法是直接用利用位运算得出结果
+	需要通过减法的位运算，
+	就是计算dividend能够减去多少个divisor
+ */
+var divide = function(dividend, divisor) {
+    
+
+
+    
+};
