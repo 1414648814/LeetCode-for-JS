546823f36bc1d74510a391f6878ebd6f94bb3296
George1994 <1414648814@users.noreply.github.com>
2016-06-12 13:25:39 +0800

    add DP

--- Climbing Stairs.js
+++ Climbing Stairs.js
@@ -20,3 +20,23 @@ var climbStairs = function(n) {
     return sum;
 
 };
+
+//动态规划
+var climbStairs2 = function(n) {
+    var result = new Array(3);
+    for(var i = 0;i < 3;i++)
+        result[i] = 0;
+    result[0] = 1;
+    result[1] = 1;
+
+    for(var i = 2;i <= n;i++)
+    {
+        result[i%3] = result[(i-1)%3] + result[(i-2)%3];
+    }
+    return result[n%3];
+
+}
+
+var n = 3;
+var result = climbStairs2(n);
+console.log(result);
