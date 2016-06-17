0ed94a572d725fe2a9e9ce7ba499596cb5203c03
George1994 <1414648814@users.noreply.github.com>
2016-06-17 23:59:08 +0800

    计算唯一的BST的数组

---
+++ Unique Binary Search Trees II.js
@@ -0,0 +1,81 @@
+
+function TreeNode(val) {
+    this.val = val;
+    this.left = this.right = null;
+}
+
+/**
+ * @param {number} n
+ * @return {TreeNode[]}
+ */
+var generateTrees = function(n) {
+    var dp = [];
+    //1.边界判断
+    if (n === 0)
+        return [];
+    else
+    {
+        //2.初始化数组
+        var dp = new Array(n + 1);
+        dp[0] = dp[1] = 1;
+        for (var i = 2;i <= n;i++)
+            dp[i] = 0;
+        for (var i = 2;i <= n; i++)
+            for (var j = 1;j <= i; j++)
+                dp[i] += dp[j-1] * dp[i-j];
+
+        //3.计算树
+        var trees = helper(1, n);
+
+        //4.计算结果
+        var res = [];
+        for (var i = 0; i < trees.length; i++)
+            res.push(trees[i]);
+        return res;
+    }
+
+    return result;
+
+    function helper(start, end) {
+        var roots = null;
+        var curlen = 0;
+
+        //1.边界判断
+        if (start > end)
+        {
+            roots = new Array(1);
+            roots[0] = null;
+            return roots;
+        }
+
+        //2.计算跟节点
+        roots = new Array(dp[end - start + 1]);
+
+        //3.计算树
+        for (var i = start; i <= end; i++)
+        {
+            //计算左子树
+            var lefts = helper(start, i-1);
+            //计算右子树
+            var rights = helper(i+1,end);
+
+            for (var p = 0; p < lefts.length; p++)
+            {
+                for (var q = 0; q < rights.length; q++)
+                {
+                    var root = new TreeNode(i);
+                    root.left = lefts[p];
+                    root.right = rights[q];
+                    roots[curlen++] = root;
+                }
+            }
+        }
+
+        return roots;
+    }
+};
+
+
+var n = 3;
+var result = generateTrees(n);
+console.log(result);
